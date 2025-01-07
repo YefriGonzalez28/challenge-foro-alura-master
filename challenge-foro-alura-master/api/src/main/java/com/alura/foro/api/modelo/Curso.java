5890363cc08047c7dcea545911815620ef1e8211
Yefri Andrey Gonzalez Acevedo <yefri77@gmail.com>
2025-01-07 17:54:28 -0500

    Add files via upload

---
+++ challenge-foro-alura-master/api/src/main/java/com/alura/foro/api/modelo/Curso.java
@@ -0,0 +1,28 @@
+package com.alura.foro.api.modelo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Table(name = "cursos")
+@Entity(name = "Curso")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String categoria;
+
+    public String getNombre() {
+        return null;
+    }
+
+    public String getCategoria() {
+        return null;
+    }
+}
